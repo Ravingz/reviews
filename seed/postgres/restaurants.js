16f24f2e2579cc9ef9a1b7b370ebfce0d51d34f3
AustinCapaviella <amcapaviella@gmail.com>
2020-04-08 01:58:16 -0700

    Complete Restaurant Seeding 10M

---
+++ seed/postgres/restaurants.js
@@ -0,0 +1,47 @@
+'use strict';
+
+  const fs = require('fs');
+  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+  const faker = require('faker');
+  const cliProgress = require('cli-progress');
+  
+  const writer = createCsvWriter({
+      path: '../../restaurants.csv',
+      header: [
+          {id: 'restaurant_id', title: 'restaurant_id'},
+          {id: 'restaurantName', title: 'restaurantName'},
+
+      ]
+  });
+  let totalRestaurants = 0;
+  let count = 100;
+  let total = 0;
+  
+  const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  bar1.start(100, 0);
+  
+  const createRestaurant = () => {
+      const restaurants = [];
+      for (let i = 0; i < count; i++) {
+          const newRestaurant= {};
+          newRestaurant.restaurant_id = totalRestaurants;
+          newRestaurant.restaurantName = faker.company.companyName();
+          totalRestaurants++; 
+          restaurants.push(newRestaurant);
+          }
+      return restaurants;
+  }
+  
+  const seeder = () => {
+      if (total < 1000) {
+          const users = createRestaurant();
+          writer.writeRecords(users)
+              .then(() => {
+                  bar1.increment(.000001)
+                  seeder()
+              })
+      total++;
+      }
+  }
+  
+  seeder();
