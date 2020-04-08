75218b90a0d5a485f8c751ec4f0a7cf8a64b638b
Austin Capaviella <59844626+amcapaviella@users.noreply.github.com>
2020-04-07 22:21:09 -0700

    Revert "Cassandra data generation"

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS ravingz_reviews;
+DROP DATABASE IF EXISTS wonder4;
 
-CREATE DATABASE ravingz_reviews;
+CREATE DATABASE wonder4;
 
-USE ravingz_reviews;
+USE wonder4;
 
 CREATE TABLE restaurants (
   id INT NOT NULL AUTO_INCREMENT,
@@ -10,22 +10,15 @@ CREATE TABLE restaurants (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE users (
-  user_id INT NOT NULL, 
-  username VARCHAR(255) NOT NULL,
-  usercity VARCHAR(255) NOT NULL,
-  avatar VARCHAR(1024) NOT NULL,
-)
-
 CREATE TABLE reviews (
   review_id INT NOT NULL AUTO_INCREMENT,
   rating INT NOT NULL,
   comment VARCHAR(2000) NOT NULL,
   date DATE NOT NULL,
-  user_id INT NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  usercity VARCHAR(255) NOT NULL,
+  avatar VARCHAR(1024) NOT NULL,
   restaurant_id INT NOT NULL,
   PRIMARY KEY (review_id),
-  FOREIGN KEY (restaurant_id) REFERENCES restaurants(id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants(id)
 );
-
