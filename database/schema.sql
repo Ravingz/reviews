17d9a3941a388fa1d9675651295abdc2f481e7d9
AustinCapaviella <amcapaviella@gmail.com>
2020-04-10 05:21:57 -0700

    database

--- database/schema.sql
+++ database/schema.sql
@@ -1,24 +1,30 @@
-DROP DATABASE IF EXISTS wonder4;
+DROP DATABASE IF EXISTS ravingz_reviews;
 
-CREATE DATABASE wonder4;
+CREATE DATABASE ravingz_reviews;
 
-USE wonder4;
+USE ravingz_reviews;
 
 CREATE TABLE restaurants (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  restaurant_id INT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE reviews (
-  review_id INT NOT NULL AUTO_INCREMENT,
-  rating INT NOT NULL,
-  comment VARCHAR(2000) NOT NULL,
-  date DATE NOT NULL,
+CREATE TABLE users (
+  user_id INT PRIMARY KEY, 
   username VARCHAR(255) NOT NULL,
   usercity VARCHAR(255) NOT NULL,
-  avatar VARCHAR(1024) NOT NULL,
+  avatar VARCHAR(1024) NOT NULL
+)
+
+CREATE TABLE reviews (
+  review_id INT PRIMARY KEY,
+  rating INT NOT NULL,
+  comment VARCHAR(2000) NOT NULL,
+  createdAt VARCHAR(40) NOT NULL,
+  updatedAt VARCHAR(40) NOT NULL,
+  user_id INT NOT NULL,
   restaurant_id INT NOT NULL,
-  PRIMARY KEY (review_id),
-  FOREIGN KEY (restaurant_id) REFERENCES restaurants(id)
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
