9f9f4f5f6c9bc99809bb2488829b7bdc5015e913
AustinCapaviella <amcapaviella@gmail.com>
2020-04-11 15:41:27 -0700

    Refactor Models To Access New DB

--- database/models.js
+++ database/models.js
@@ -1,9 +1,8 @@
 const db = require('../database')
 
-
 const getRecentReviews = (callback) => {
   //get most recent 15 reviews
-  let queryString = 'SELECT * FROM reviews ORDER BY date DESC LIMIT 15';
+  let queryString = 'SELECT * FROM reviews ORDER BY createdAt DESC LIMIT 15';
   db.query(queryString, (err, results) => {
     if (err) {
       console.log('error getting recent reviews from database', err)
@@ -16,7 +15,7 @@ const getRecentReviews = (callback) => {
 
 const getRestaurantName = (id, callback) => {
   //get the restrant by restaurant id
-  let queryString = `SELECT name FROM restaurants WHERE id=${id}`;
+  let queryString = `SELECT name FROM restaurants WHERE restaurant_id=${id}`;
   db.query(queryString, (err, result) => {
     if (err) {
       console.log('error getting restaurant name from database', err)
@@ -29,12 +28,11 @@ const getRestaurantName = (id, callback) => {
 
 const getReviewsByRestaurant = (id, callback) => {
   // get All the reviews by a resturant id
-  let queryString = `SELECT * FROM reviews INNER JOIN restaurants ON reviews.restaurant_id = restaurants.id WHERE reviews.restaurant_id = ${id} ORDER BY date DESC`
+  let queryString = `SELECT * FROM reviews INNER JOIN users ON users.user_id = reviews.user_id WHERE reviews.restaurant_id = ${id} ORDER BY updatedat DESC`
   db.query(queryString, (err, results) => {
     if (err) {
       console.log('error getting reviews by restaurant from database', err)
     } else {
-      // console.log(results);
       callback(null, results);
     }
   });
@@ -55,3 +53,4 @@ const insertReview = (review, callback) => {
 }
 
 module.exports = { getRecentReviews, getRestaurantName, getReviewsByRestaurant, insertReview };
+
